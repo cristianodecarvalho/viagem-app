6b7dfa424450c01a4a1439583b9a0ea9e47cda01
Cristiano de Carvalho Martins Filho <cristianfilho18@gmail.com>
2022-11-29 12:11:21 -0300

    Feita a separação dos serviços Cliente e Reserva

---
+++ agencia-micro-reserva/src/main/java/com/cristianofilho/reserva/Reserva.java
@@ -0,0 +1,12 @@
+package com.cristianofilho.reserva;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Reserva extends PanacheEntity {
+    
+    public long idCliente;
+
+}
