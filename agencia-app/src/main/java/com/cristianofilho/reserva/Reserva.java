6aad6fd8ace81f076a2ae74f57c722858c1f4738
Cristiano de Carvalho Martins Filho <cristianfilho18@gmail.com>
2022-11-29 10:08:06 -0300

    Criando as classes de Cliente e Reserva

---
+++ agencia-app/src/main/java/com/cristianofilho/reserva/Reserva.java
@@ -0,0 +1,17 @@
+package com.cristianofilho.reserva;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+import com.cristianofilho.cliente.Cliente;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Reserva extends PanacheEntity {
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Cliente cliente;
+
+}
